bd641a5664e87b783ae9287c49e4e5205e82cff5
tronak19 <tronak19@gmail.com>
2017-11-18 17:21:44 -0500

    Updated Groupby code

--- groupby.py
+++ groupby.py
@@ -4,17 +4,18 @@ import sklearn as sk
 import pickle
 
 df = pd.read_csv("h1b_kaggle.csv")
-df.dropna()
+df = df.rename(columns={'Unnamed: 0': 'ID'})
+df['FULL_TIME_POSITION'] = np.where(df['FULL_TIME_POSITION']=="Y",1,0)
 df = df.drop(['lon', 'lat'], axis=1)
+df = df.dropna(subset=['JOB_TITLE'])
+df['YEAR'] = df['YEAR'].astype(int)
 df = df.loc[df['CASE_STATUS'] == 'CERTIFIED']
-df.dropna(subset=['JOB_TITLE'], inplace = True)
-
-df = df.rename(columns={'Unnamed: 0': 'Serial_Number'})
-df.drop(['Serial_Number'], axis=1)
+df = df.drop(['CASE_STATUS'], axis=1)
+df = df.drop(['ID'], axis=1)
 df = df.reset_index(drop=True)
-df = df.set_index('Serial_Number')
-data = df.groupby('JOB_TITLE')
-print(list(data))
 
-with open("groupby2.pkl", "wb") as f:
-    pickle.dump(data, f)
+grouped_data = df.groupby('JOB_TITLE')
+print(list(grouped_data))
+
+with open("grouped_data.pkl", "wb") as f:
+    pickle.dump(grouped_data, f)
